74e4c716d153a45f1721c503fdb1d4ef6d1f0b2d
Jimmy Chen <jimmy600d@gmail.com>
2017-06-16 11:13:36 +0700

    test13

---
+++ common/models/test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = function(Test) {
+var schema_v1 = {
+  "name": "test",
+  "options": {
+    "idInjection": false,
+    "mysql": {
+      "schema": "compose",
+      "table": "TEST"
+    }
+  },
+  "properties": {
+    "id": {
+      "type": "string",
+      "length": 50,
+      "id": true
+    },
+    "name": {
+      "type": "string",
+      "length": 50,
+      "required": true
+    },
+    "address": {
+      "type": "string",
+      "length": 50,
+      "required": true
+    }
+  }
+};
+	var loopback = require('loopback');
+	var app = loopback();
+	var DataSource = require('loopback-datasource-juggler').DataSource;
+	var ds = new DataSource({
+		connector: require('loopback-connector-mysql'),
+		host: 'sl-us-south-1-portal.1.dblayer.com',
+		port: 17034,
+		database: 'compose',
+		username: 'admin',
+		password: 'DTNXDTJQNQXDYVAZ',
+	});
+	// var Model = app.models();
+	// var ds = app.dataSource('db');
+
+	ds.createModel(schema_v1.name, schema_v1.properties, schema_v1.options);
+
+	ds.autoupdate(function () {
+	  ds.discoverModelProperties('TEST', function (err, props) {
+	    console.log(props);
+	  });
+	});
+
+};
